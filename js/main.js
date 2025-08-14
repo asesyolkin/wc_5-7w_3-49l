074ee85f701967b6e632230e96abb2a55e198571
Aleksandr Sesyolkin <sesyolkin@gmail.com>
2025-08-14 15:32:16 +0300

    added the custom controls for the slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 /* Tiny slider */
 
+// Инициализируем слайдер и настраиваем его
 const slider = tns({
   container: '.slider',
-  items: 3,
-  slideBy: 'page',
-  autoplay: true,
+  items: 2.7,
+  gutter: 10,
   mouseDrag: true,
-  gutter: 20
+  speed: 400,
+  loop: false,
+  autoWidth: true,
+  controlsContainer: '.locations__controls',
+  nav: false
 });
+
+// Кнопки управления слайдером
+const prevMobile = document.querySelector('.locations__controls--mobile .prev');
+const nextMobile = document.querySelector('.locations__controls--mobile .next');
+
+// Обращаемся к кнопкам и по событию клика вешаем на них методы перелистывания слайдера вперед и назад
+prevMobile.onclick = () => slider.goTo('prev');
+nextMobile.onclick = () => slider.goTo('next');
